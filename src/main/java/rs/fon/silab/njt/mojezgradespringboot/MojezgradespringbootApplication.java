6a3801d9817c776ed8c0f2779b1c00eadcd3671b
Sara-Petrovic <sarapetrovicue@gmail.com>
2021-06-24 09:56:27 +0200

    Kreiran SpringBoot projekat.

---
+++ src/main/java/rs/fon/silab/njt/mojezgradespringboot/MojezgradespringbootApplication.java
@@ -0,0 +1,13 @@
+package rs.fon.silab.njt.mojezgradespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojezgradespringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojezgradespringbootApplication.class, args);
+	}
+
+}
