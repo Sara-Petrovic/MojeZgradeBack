5c9b91b3d731516fd259e3725b01883a64488101
AleksandraMisic762 <am20170003@student.fon.bg.ac.rs>
2021-06-25 20:49:45 +0200

    Dodate kontroler, repozitorijum i service klase za Racun

---
+++ src/main/java/rs/fon/silab/njt/mojezgradespringboot/service/RacunService.java
@@ -0,0 +1,13 @@
+package rs.fon.silab.njt.mojezgradespringboot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import rs.fon.silab.njt.mojezgradespringboot.repository.RacunRepository;
+
+@Service
+@Transactional
+public class RacunService {
+    @Autowired
+    private RacunRepository repo;
+}
