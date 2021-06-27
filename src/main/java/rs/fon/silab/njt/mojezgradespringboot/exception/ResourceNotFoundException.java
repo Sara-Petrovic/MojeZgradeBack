a9072eaac843ca9f5bd0f2e13aec7e86cfa3e23d
Sara-Petrovic <sarapetrovicue@gmail.com>
2021-06-27 20:18:53 +0200

    Dodat Exception handling.

--- src/main/java/rs/fon/silab/njt/mojezgradespringboot/exception/ResourceNotFoundException.java
+++ src/main/java/rs/fon/silab/njt/mojezgradespringboot/exception/ResourceNotFoundException.java
@@ -1,10 +1,15 @@
 package rs.fon.silab.njt.mojezgradespringboot.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends Exception{
 
-    public ResourceNotFoundException(String string) {
-        super(string);
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message){
+        super(message);
     }
-    
 }
