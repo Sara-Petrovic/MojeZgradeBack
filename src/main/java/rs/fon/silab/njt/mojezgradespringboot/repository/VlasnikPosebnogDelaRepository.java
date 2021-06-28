f7ccf50133645232861fcc1936443538ad773633
Sara-Petrovic <sarapetrovicue@gmail.com>
2021-06-28 12:31:49 +0200

    Dodat repo i service za pretragu vlasnika po prezimenu.

--- src/main/java/rs/fon/silab/njt/mojezgradespringboot/repository/VlasnikPosebnogDelaRepository.java
+++ src/main/java/rs/fon/silab/njt/mojezgradespringboot/repository/VlasnikPosebnogDelaRepository.java
@@ -1,9 +1,12 @@
 package rs.fon.silab.njt.mojezgradespringboot.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import rs.fon.silab.njt.mojezgradespringboot.model.VlasnikPosebnogDela;
 
 
 public interface VlasnikPosebnogDelaRepository extends JpaRepository<VlasnikPosebnogDela, Long>{
-    
+    //@Query("SELECT u FROM User u WHERE u.status = 1")
+    List<VlasnikPosebnogDela> findByPrezimeContaining(String prezime);
 }
