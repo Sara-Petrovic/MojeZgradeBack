cee9dbc9dfc1ecf3d8f7f400b144ab6901f70d1d
Sara-Petrovic <sarapetrovicue@gmail.com>
2021-06-28 15:51:41 +0200

    Kreiran kontroler za SednicuSkupstine.

---
+++ src/main/java/rs/fon/silab/njt/mojezgradespringboot/controller/SednicaSkupstineController.java
@@ -0,0 +1,42 @@
+package rs.fon.silab.njt.mojezgradespringboot.controller;
+
+import java.util.List;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import rs.fon.silab.njt.mojezgradespringboot.exception.ResourceNotFoundException;
+import rs.fon.silab.njt.mojezgradespringboot.model.SednicaSkupstine;
+import rs.fon.silab.njt.mojezgradespringboot.model.VlasnikPosebnogDela;
+import rs.fon.silab.njt.mojezgradespringboot.service.SednicaSkupstineService;
+import rs.fon.silab.njt.mojezgradespringboot.service.VlasnikPosebnogDelaService;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class SednicaSkupstineController {
+    @Autowired
+    private SednicaSkupstineService service;
+
+    @PostMapping("/sednicaskupstine")
+    public SednicaSkupstine saveSednicaSkupstine(@Valid @RequestBody SednicaSkupstine sednica) {
+        return service.save(sednica);
+    }
+    
+    @GetMapping("/sednicaskupstine")
+    public List<SednicaSkupstine> getAllSednicaSkupstine() {
+        return service.findAll();
+    }
+
+    @GetMapping("/sednicaskupstine/{id}")
+    public ResponseEntity<SednicaSkupstine> getSednicaSkupstineById(@PathVariable(value = "id") Long sednicaId)
+         throws ResourceNotFoundException {
+        SednicaSkupstine s = service.findById(sednicaId)
+                .orElseThrow(() -> new ResourceNotFoundException("Ne postoji sednica skupstine sa ovim id-jem :: " + sednicaId));
+        return ResponseEntity.ok().body(s);
+    }
+}
