11e81e3d2e770eb8854b4211f645742d122c7635
Sara-Petrovic <sarapetrovicue@gmail.com>
2021-06-28 15:51:57 +0200

    Kreiran repository za SednicuSkupstine.

---
+++ src/main/java/rs/fon/silab/njt/mojezgradespringboot/repository/SednicaSkupstineRepository.java
@@ -0,0 +1,9 @@
+package rs.fon.silab.njt.mojezgradespringboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import rs.fon.silab.njt.mojezgradespringboot.model.SednicaSkupstine;
+
+
+public interface SednicaSkupstineRepository extends JpaRepository<SednicaSkupstine, Long>{
+    
+}
