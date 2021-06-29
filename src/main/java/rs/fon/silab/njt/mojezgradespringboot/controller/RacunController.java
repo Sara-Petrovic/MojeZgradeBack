1680fb3981115b0ce66226a8c827a587b9999c60
AleksandraMisic762 <am20170003@student.fon.bg.ac.rs>
2021-06-29 19:17:35 +0200

    Implementirano cuvanje racuna

--- src/main/java/rs/fon/silab/njt/mojezgradespringboot/controller/RacunController.java
+++ src/main/java/rs/fon/silab/njt/mojezgradespringboot/controller/RacunController.java
@@ -2,7 +2,10 @@ package rs.fon.silab.njt.mojezgradespringboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import rs.fon.silab.njt.mojezgradespringboot.model.Racun;
 import rs.fon.silab.njt.mojezgradespringboot.service.RacunService;
 
 @RestController
@@ -10,5 +13,11 @@ import rs.fon.silab.njt.mojezgradespringboot.service.RacunService;
 public class RacunController {
     @Autowired
     private RacunService service;
+    
+    @PostMapping("/racun")
+    public Racun saveStambenaZajednica(@RequestBody Racun r){
+        return service.save(r);
+    }
+
     
 }
--- src/main/java/rs/fon/silab/njt/mojezgradespringboot/service/RacunService.java
+++ src/main/java/rs/fon/silab/njt/mojezgradespringboot/service/RacunService.java
@@ -3,6 +3,7 @@ package rs.fon.silab.njt.mojezgradespringboot.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import rs.fon.silab.njt.mojezgradespringboot.model.Racun;
 import rs.fon.silab.njt.mojezgradespringboot.repository.RacunRepository;
 
 @Service
@@ -10,4 +11,8 @@ import rs.fon.silab.njt.mojezgradespringboot.repository.RacunRepository;
 public class RacunService {
     @Autowired
     private RacunRepository repo;
+
+    public Racun save(Racun r) {
+        return repo.save(r);
+    }
 }
