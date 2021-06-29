bf4ca7a15bef225692e5da6ffe100a1b3d703d2a
AleksandraMisic762 <am20170003@student.fon.bg.ac.rs>
2021-06-29 19:28:29 +0200

    Implementirana izmena racuna

--- src/main/java/rs/fon/silab/njt/mojezgradespringboot/controller/RacunController.java
+++ src/main/java/rs/fon/silab/njt/mojezgradespringboot/controller/RacunController.java
@@ -1,8 +1,13 @@
 package rs.fon.silab.njt.mojezgradespringboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import rs.fon.silab.njt.mojezgradespringboot.model.Racun;
@@ -15,9 +20,30 @@ public class RacunController {
     private RacunService service;
     
     @PostMapping("/racun")
-    public Racun saveStambenaZajednica(@RequestBody Racun r){
+    public Racun saveRacun(@RequestBody Racun r){
         return service.save(r);
     }
+    
+    @GetMapping("/racun/{id}")
+    public ResponseEntity<?> findRacun(@PathVariable Long id){
+        Racun r =  service.find(id);
+        if(r == null){
+            return new ResponseEntity<>("Racun sa ovim id nije pronadjen.",HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok().body(r);
+    }
 
+    @PutMapping("/racun/{id}")
+    public ResponseEntity<?> updateRacun(@PathVariable Long id, @RequestBody Racun racun) throws Exception{
+        Racun updatedRacun =  service.find(id);
+        if(updatedRacun == null){
+            throw new Exception();
+        }
+        updatedRacun.setStatus(racun.getStatus());
+        /*
+            nije moguce da se menja kog datuma je izdat racun, na koga glasi ili na koju sumu
+        */
+        return ResponseEntity.ok().body(service.save(updatedRacun));
+    }
     
 }
--- src/main/java/rs/fon/silab/njt/mojezgradespringboot/service/RacunService.java
+++ src/main/java/rs/fon/silab/njt/mojezgradespringboot/service/RacunService.java
@@ -1,5 +1,6 @@
 package rs.fon.silab.njt.mojezgradespringboot.service;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,4 +16,14 @@ public class RacunService {
     public Racun save(Racun r) {
         return repo.save(r);
     }
+
+    public Racun find(Long id) {
+        Optional<Racun> optRacun = repo.findById(id);
+        if(!optRacun.isPresent()){
+            return null;
+        }
+        return optRacun.get();
+    }
+    
+    
 }
